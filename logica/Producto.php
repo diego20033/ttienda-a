821a5a7d184da09593eb763a4d05661e6ea54005
diego20033 <diaguevaraa@udistrital.edu.co>
2024-09-24 13:42:43 -0500

    tarea r

--- logica/Producto.php
+++ logica/Producto.php
@@ -1,76 +1,61 @@
 <?php
 require_once ("./persistencia/Conexion.php");
-require ("./persistencia/ProductoDAO.php");
+require_once ("./persistencia/ProductoDAO.php");
+require_once ("./persistencia/MarcaDAO.php");
 
-class Producto{
+class Producto {
     private $idProducto;
     private $nombre;
     private $cantidad;
     private $precioCompra;
     private $precioVenta;
+    private $marca;  
+    private $categoria; 
 
-    public function getIdProducto() {
-        return $this->idProducto;
-    }
-
-    public function getNombre() {
-        return $this->nombre;
-    }
-
-    public function getCantidad() {
-        return $this->cantidad;
-    }
-
-    public function getPrecioCompra () {
-        return $this->precioCompra;
-    }
-
-    public function getPrecioVenta () {
-        return $this->precioVenta;
-    }
-
-    public function setIdProducto($idProducto){
+    public function __construct($idProducto=0, $nombre="", $cantidad=0, $precioCompra=0, $precioVenta=0, $marca="", $categoria="") {
         $this->idProducto = $idProducto;
-    }
-
-    public function setNombre($nombre){
         $this->nombre = $nombre;
-    }
-
-    public function setCantidad($cantidad){
         $this->cantidad = $cantidad;
-    }
-
-    public function setPrecioCompra($precioCompra){
         $this->precioCompra = $precioCompra;
-    }
-
-    public function setPrecioVenta($precioVenta){
         $this->precioVenta = $precioVenta;
+        $this->marca = $marca; 
+        $this->categoria = $categoria;
     }
 
-    public function __construct($idProducto=0, $nombre="", $cantidad=0, $precioCompra=0, $precioVenta=0){
-        $this -> idProducto = $idProducto;
-        $this -> nombre = $nombre;
-        $this -> cantidad = $cantidad;
-        $this -> precioCompra = $precioCompra;
-        $this -> precioVenta = $precioVenta;
-    }
     
-    public function consultarTodos(){
+    public function getMarca() {
+        return $this->marca;
+    }
+
+    public function getCategoria() {
+        return $this->categoria;
+    }
+
+    public function consultarTodos() {
         $productos = array();
         $conexion = new Conexion();
-        $conexion -> abrirConexion();
+        $conexion->abrirConexion();
+        
         $productoDAO = new ProductoDAO();
-        $conexion -> ejecutarConsulta($productoDAO -> consultarTodos());
-        while($registro = $conexion -> siguienteRegistro()){
+        $marcaDAO = new MarcaDAO();
+        
+       
+        $conexion->ejecutarConsulta($productoDAO->consultarTodos());
+
+       
+        while ($registro = $conexion->siguienteRegistro()) {
             $producto = new Producto($registro[0], $registro[1], $registro[2], $registro[3], $registro[4]);
+
+          
+            $conexion->ejecutarConsulta($marcaDAO->consultarPorId($registro[5]));
+            $marcaRegistro = $conexion->siguienteRegistro();
+            $producto->marca = $marcaRegistro[1];  
+            
             array_push($productos, $producto);
         }
-        $conexion -> cerrarConexion();
-        return $productos;        
-    }
-    
-}
 
+        $conexion->cerrarConexion();
+        return $productos;
+    }
+}
 ?>
--- persistencia/ProductoDAO.php
+++ persistencia/ProductoDAO.php
@@ -1,25 +1,28 @@
 <?php
-class ProductoDAO{
+class ProductoDAO {
     private $idProducto;
     private $nombre;
     private $cantidad;
     private $precioCompra;
     private $precioVenta;
     
-    public function __construct($idProducto=0, $nombre="", $cantidad=0, $precioCompra=0, $precioVenta=0){
-        $this -> idProducto = $idProducto;
-        $this -> nombre = $nombre;
-        $this -> cantidad = $cantidad;
-        $this -> precioCompra = $precioCompra;
-        $this -> precioVenta = $precioVenta;
+    public function __construct($idProducto=0, $nombre="", $cantidad=0, $precioCompra=0, $precioVenta=0) {
+        $this->idProducto = $idProducto;
+        $this->nombre = $nombre;
+        $this->cantidad = $cantidad;
+        $this->precioCompra = $precioCompra;
+        $this->precioVenta = $precioVenta;
     }
-    
-    public function consultarTodos(){
-        return "select idProducto, nombre, cantidad, precioCompra, precioVenta 
-                from Producto";
+
+    public function consultarTodos() {
+        return "
+            SELECT p.idProducto, p.nombre, p.cantidad, p.precioCompra, p.precioVenta, m.nombre AS marca, c.nombre AS categoria
+            FROM Producto p
+            JOIN Marca m ON p.Marca_idMarca = m.idMarca
+            JOIN Categoria c ON p.Categoria_idCategoria = c.idCategoria
+            ORDER BY p.nombre ASC
+        ";
     }
-    
     
 }
-
 ?>
